85330dc5c650a5074ea3225d70184cf330e60f96
Rahul Bisht <147092147+RahulBisht45@users.noreply.github.com>
2023-12-11 16:05:50 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+//let score=35;
+let score= true;
+
+//console.log(typeof score);
+ let valueInNumber=Number(score);
+
+ //console.log(typeof valueInNumber);
+
+ //console.log(valueInNumber);
+
+
+ //"35"=> 35
+ //"35ac"=> NaN
+ //true=> 1;
+
+ let login="rahul";
+ let booleanLogin=Boolean(login);
+ console.log(booleanLogin);
+
+ //1=>true;
+ // "rahul"=>true;  ""=>false
+ 
