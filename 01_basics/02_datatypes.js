d38341e241d334cf14a1c7c9bf1eed6e8bda66c9
Rahul Bisht <147092147+RahulBisht45@users.noreply.github.com>
2023-12-11 13:29:54 +0000

    config

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use script"; // treat all js code as newer version
+
+//alert //we are using nodejs not browser\
+
+//number=> 2 to the power 53
+//bigint
+//string
+//boolean
+//null=> standalone value
+//undefined
+//symbol
+
+let age=26;
+let temp= null;
+let value= undefined;
+let name= "rohan";
+ console.log(typeof temp);   //output- object
---
+++ 01_basics/variable.js
@@ -0,0 +1,14 @@
+const id=123;
+var name="rahul";
+let age=25;
+city='mumbai';
+//id=456;
+console.log(id);
+
+console.log(city);
+/*
+prefer not to use var keywords becoz issue in block scope
+
+*/
+
+console.table([id,name,age]);
