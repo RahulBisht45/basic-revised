b4f319672e6a93a709090e85be97a872cf651065
Rahul Bisht <147092147+RahulBisht45@users.noreply.github.com>
2023-12-12 14:07:22 +0000

    string config

---
+++ 01_basics/04_string.js
@@ -0,0 +1,18 @@
+const name="rahul";
+const age=25;
+
+
+console.log(`hello ${name} -${age}`);
+
+const newName=new String('rahulbro');
+
+console.log(newName.__proto__);
+
+console.log(newName.charAt(3));
+console.log(newName.indexOf('h'));
+console.log(newName.substring(0,5));
+console.log(newName.slice(-6,4));
+
+const string1="  rahull   ";
+console.log(string1);
+console.log(string1.trim());
