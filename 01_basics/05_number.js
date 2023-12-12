dc66bab86d0b7313d895727c722151698cb3ae51
Rahul Bisht <147092147+RahulBisht45@users.noreply.github.com>
2023-12-12 14:47:11 +0000

    no and math

---
+++ 01_basics/05_number.js
@@ -0,0 +1,21 @@
+const score= new Number(300.585);
+console.log(score);
+
+console.log(score.toString().length);
+console.log(score.toFixed(2));
+
+const nextScore=125.84569;
+console.log(nextScore.toPrecision(3));
+
+const hundreds=100000000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+//************************** */
+
+
+console.log(Math.abs(-45))
+console.log(Math.round(4.5));
+
+console.log((Math.random()*10)+1);
