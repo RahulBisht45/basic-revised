5fc339d53221f96f3414a7a03d5669ddc3ef26ea
Rahul Bisht <147092147+RahulBisht45@users.noreply.github.com>
2023-12-13 15:21:42 +0000

    array methods

---
+++ 02_basics/01_array.js
@@ -0,0 +1,36 @@
+const myArr=[1,2,3,4,5,6,7];
+myArr.push(9);
+
+//console.log(myArr);
+
+//myArr.pop();
+//myArr.unshift(0);
+//console.log(myArr);
+//myArr.shift();
+//console.log(myArr);
+
+//console.log(myArr.includes(8));
+//console.log(myArr.indexOf(10));
+
+//const newArr=myArr.join()
+//console.log(myArr);
+//console.log(newArr);
+//console.log(typeof newArr);
+
+
+//slice, splice
+
+console.log("A", myArr);
+
+const arr1=myArr.slice(1,5);
+console.log(arr1);
+
+console.log("B", myArr);
+
+
+
+const arr2=myArr.splice(1,5);
+console.log(arr2);
+
+console.log("C", myArr);
+
