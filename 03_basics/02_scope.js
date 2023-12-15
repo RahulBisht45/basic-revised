8d8824f232d54b8bb5e3fc6b6dc06d94861352f5
Rahul Bisht <147092147+RahulBisht45@users.noreply.github.com>
2023-12-15 16:51:18 +0000

    scope config

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,39 @@
+function one(){
+    const userName="rahul"
+
+    function two()
+    {
+       const age=25
+    console.log(userName);
+    }
+    //console.log(age);
+    two();
+
+}
+one();
+
+if(true){
+    user="Roshan"
+    if(true){
+       let age=26
+    console.log(`hey ${user} how r U`);
+    }
+
+    //console.log(`ur age is ${age}` );
+
+}
+
+
+ //++++++++++++++++ interesting +++++++++++++
+
+
+ console.log(addfive(7))
+ function addfive(num){
+    return num+5
+ }
+
+ addten(15)   // hoisting takes place here
+const addten=function(num){
+    return num+10
+}
+
