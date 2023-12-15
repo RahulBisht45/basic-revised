85312b3c90ab03b47a4f4a38d49c8ad3c3c81587
Rahul Bisht <147092147+RahulBisht45@users.noreply.github.com>
2023-12-15 15:56:59 +0000

    func with object and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,4 +27,36 @@ function loginUser(userName){
 }
 
 //console.log(loginUser("rahul"));
-console.log(loginUser());
+//console.log(loginUser());
+
+
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1     // it will return [400,500]
+}
+//console.log(calculateCartPrice(2))
+ console.log(calculateCartPrice(200,300,400,500))
+
+ const user={
+    userName:"Rahul",
+    age:25
+}
+
+function handleObject(anyobject){
+    console.log(`user name is ${anyobject.userName} and age is ${anyobject.age}`);
+
+}
+//handleObject(user);
+
+handleObject({
+    userName:"ROHAN",
+    age:26
+})
+
+const myNewArr=[100,300,500,700]
+
+function returnThirdvalue(getArray){
+    return getArray[2]
+}
+//console.log(returnThirdvalue(myNewArr));
+console.log(returnThirdvalue([300,500,700,800]));
