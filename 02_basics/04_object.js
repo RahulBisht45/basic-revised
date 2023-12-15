810fa173c54fe4e29ad14f4ced2ac09d2b403754
Rahul Bisht <147092147+RahulBisht45@users.noreply.github.com>
2023-12-15 05:51:27 +0000

    objects config

---
+++ 02_basics/04_object.js
@@ -0,0 +1,41 @@
+//singleton
+
+
+//const instaUser=new Object()
+const instaUser={}
+
+instaUser.id="1212bc"
+instaUser.name="harry"
+instaUser.isLoggedIn=false
+
+
+//console.log(instaUser);
+
+const regUser={
+    email:"hey@gmail.com",
+    fullname:{
+        userName:{
+            fname:"rahul",
+            Lname:"bisht"
+        }
+    }
+}
+
+//console.log(regUser.fullname.userName.Lname);
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+
+//const obj3=Object.assign({},obj1,obj2)
+
+const obj3={...obj1,...obj2}
+console.log(obj3);
+
+console.log(instaUser);
+
+console.log(Object.keys(instaUser));
+console.log(Object.values(instaUser));
+console.log(Object.entries(instaUser));
+
+
+console.log(instaUser.hasOwnProperty('isLoggedIn'));
