2fcd3aed2f071071462f89de219cff146bf3b26c
Rahul Bisht <147092147+RahulBisht45@users.noreply.github.com>
2023-12-20 15:26:44 +0000

    break and continue in for loop

---
+++ 04_iteration/forloop.js
@@ -0,0 +1,17 @@
+//break and continue
+
+/*for (let i = 0; i <=10; i++) {
+    if (i==6) {
+        console.log("Detected 6");
+     break
+    }
+    console.log(`value of i is ${i}`);
+}*/
+
+for (let i = 0; i <=10; i++) {
+    if (i==6) {
+        console.log("Detected 6");
+     continue
+    }
+    console.log(`value of i is ${i}`);
+}
